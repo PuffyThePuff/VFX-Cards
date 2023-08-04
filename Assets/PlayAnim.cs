e75aed3065ec167de0e48214333633b06b97dcc8
IncogLad <vincenthe5th@yahoo.com>
2023-08-05 00:26:54 +0800

    .

--- Assets/PlayAnim.cs
+++ Assets/PlayAnim.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayAnim : MonoBehaviour
 {
     [SerializeField] public Animator CardAnimAnimator;
+    [SerializeField] public Text FPSCOunter;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,7 @@ public class PlayAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        FPSCOunter.text = (1.0f/Time.deltaTime).ToString();
     }
 
     public void PlayAnimation(string name) 
