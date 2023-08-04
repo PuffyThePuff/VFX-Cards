0fa081b93b4e4b6c5154922309923e7019c66f9c
IncogLad <vincenthe5th@yahoo.com>
2023-08-05 00:41:57 +0800

    big maybe

--- Assets/PlayAnim.cs
+++ Assets/PlayAnim.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayAnim : MonoBehaviour
 {
     [SerializeField] public Animator CardAnimAnimator;
-    [SerializeField] public Text FPSCOunter;
+    [SerializeField] public TextMeshProUGUI FPSCOunter;
+    private float tickInterval = 100f;
+
+    private float ticks = 0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +22,19 @@ public class PlayAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        FPSCOunter.text = (1.0f/Time.deltaTime).ToString();
+        ticks++;
+        if (ticks >= tickInterval)
+        {
+            float fps = 1.0f / Time.deltaTime;
+            if (fps > 60f)
+            {
+                //fps = 60f;
+            }
+            FPSCOunter.text = "FPS: " + fps.ToString("F0");
+            ticks = 0;
+        }
+
+       
     }
 
     public void PlayAnimation(string name) 
